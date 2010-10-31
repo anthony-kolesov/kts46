e296b3cf1687a27a79f3fcd23ac2e78d646b5f69
Anthony Kolesov <anthony.kolesov@gmail.com>
2010-10-31 17:40:32 +0400

    Move CouchDB storage in separate file to pylib.

--- gui/components/RoadNetworkModel.py
+++ gui/components/RoadNetworkModel.py
@@ -1,13 +1,13 @@
 import logging, sqlite3, couchdb, json
 from xpcom import components
-from roadModel import Car, Road, SimpleSemaphore, Model
+from roadModel import Car, Road, SimpleSemaphore, Model, CouchDBStorage
 
 class MozConsoleHandler(logging.Handler):
-    
+
     def __init__(self, level=logging.NOTSET):
         logging.Handler.__init__(self, level)
         self._log = self._log = components.classes['@mozilla.org/consoleservice;1'].getService(components.interfaces.nsIConsoleService)
-        
+
     def emit(self, record):
         self._log.logStringMessage(record.getMessage())
 
@@ -20,8 +20,8 @@ class SQLiteStorage:
         # DB schema
         self.cur.execute("DROP TABLE IF EXISTS states;")
         self.cur.execute("CREATE TABLE states (time float, state text);")
-        self.dump() # Fix changes to scheme and start transaction.  
-        
+        self.dump() # Fix changes to scheme and start transaction.
+
     def open(self, path):
         try:
             conn = sqlite3.connect(path)
@@ -30,51 +30,18 @@ class SQLiteStorage:
             raise
         cur = conn.cursor()
         return (conn, cur)
-        
+
     def add(self, time, data):
         self.cur.execute('INSERT INTO states VALUES(:time, :state);',
                 {'time': time, 'state': json.dumps(data)} )
-    
+
     def dump(self):
         self.conn.commit() # Dump previous transaction.
         self.cur.execute('BEGIN TRANSACTION;') # And begin new.
-    
+
     def close(self):
         self.conn.commit() # Close TX.
         self.conn.close()
-    
-        
-class CouchDBStorage:
-
-    def __init__(self, servername, dbname, bufferSize = 1000):
-        self.logger = logging.getLogger('roadModel.couchdb-storage')
-        self.server = couchdb.Server(servername)
-        if dbname not in self.server:
-            self.db = self.server.create(dbname)
-        else:
-            self.db = self.server[dbname]
-        self.bulk_queue = []
-        self.bufferSize = bufferSize
-        
-    def add(self, time, data):
-        d = dict(data)
-        d['time'] = time
-        d['type'] = 'state'
-        d['_id'] = 'st_'+str(time)
-        doc = couchdb.Document(d)
-        
-        self.bulk_queue.append(doc)
-        if len(self.bulk_queue) >= self.bufferSize:
-            self.dump()
-    
-    def dump(self):
-        if len(self.bulk_queue) > 0:
-            self.db.update(self.bulk_queue)
-            self.bulk_queue = []
-    
-    def close(self):
-        # couchdb for python doesn't have any 'close' methods, so just dump content.
-        self.dump()    
 
 
 class RoadNetworkModel:
@@ -91,7 +58,7 @@ class RoadNetworkModel:
 
     def run_step(self, milliseconds):
         self._model.run_step(milliseconds)
-        
+
     def get_state_data(self):
         return json.dumps(self._model.get_state_data())
 
@@ -107,7 +74,7 @@ class RoadNetworkModel:
         stepsN = duration / step
         stepsCount = 0
         t = 0.0
-        
+
         # Prepare infrastructure.
         logger = logging.getLogger('roadModel.manager')
         try:
@@ -116,13 +83,9 @@ class RoadNetworkModel:
         except sqlite3.OperationalError:
             logger.error("Couldn't open database file: %s." % outpath)
             return
-        #cur = conn.cursor()
         # DB schema
-        #cur.execute("DROP TABLE IF EXISTS states;")
-        #cur.execute("CREATE TABLE states (float time, string state);")
-        #conn.commit()
         logger.info('stepsN: %i, stepsCount: %i, stepsN/100: %i', stepsN, stepsCount, stepsN / 100)
-        
+
         # Run.
         while t < duration:
             self.run_step(stepAsMs)
@@ -130,19 +93,14 @@ class RoadNetworkModel:
             # Report every percent.
             if stepsCount % (stepsN / 100) == 0:
                 reporter.report(t, duration)
-            ## Dump every 100 tx
-            #if stepsCount % 100 == 0:
-            #    storage.dump()
             # Round time to milliseconds
             storage.add(round(t, 3),  self._model.get_state_data())
             t += step
-            
+
         # Finilize.
         storage.close()
         reporter.report(1.0, 1.0) # Report exactly 100%
-        #conn.commit()
-        #conn.close()
-        
+
 # Add XPCOM log handler.
 # Set up logging to file. Logging to XULRunner console will initialized automatically.
 logging.basicConfig(level=logging.DEBUG,
@@ -151,4 +109,3 @@ logging.basicConfig(level=logging.DEBUG,
                     filename='/tmp/rns-xul-runner.log',
                     filemode='w')
 logging.getLogger('').addHandler(MozConsoleHandler())
-
