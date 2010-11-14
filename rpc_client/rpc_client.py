3f91d38a2f6caca9a2cdd9ecdb41bf8512c617d7
Anthony Kolesov <anthony.kolesov@gmail.com>
2010-11-14 22:04:52 +0400

    Fix errors in RPC client.

--- rpc_client/rpc_client.py
+++ rpc_client/rpc_client.py
@@ -39,18 +39,18 @@ def init():
     console.setFormatter( logging.Formatter(cfg.get('log', 'shortFormat')) )
     logger = logging.getLogger(cfg.get('log', 'loggerName'))
     logger.addHandler(console)
-    
+
     return (cfg, logger)
 
 if __name__ == '__main__':
     cfg, logger = init()
-    
+
     # create project
     # delete project
     # add job
     # delete job
     # run job
-    
+
     usage = "usage: %prog [options] <action>"
     epilog = """<action> could be: createProject (crp), deleteProject (delp)
 addJob (a), deleteJob (delj), runJob (r)."""
@@ -60,14 +60,14 @@ addJob (a), deleteJob (delj), runJob (r)."""
     cmdOpts.add_option('-j', '--job', action='store', dest='job', default='',
                        help='Job name.')
     cmdOpts.add_option('-f', '--file', action='store', dest='file', default='',
-                       help='Path to file with job definition.')              
+                       help='Path to file with job definition.')
 
     options, args = cmdOpts.parse_args(sys.argv[1:])
     if len(options.proj) < 1:
         cmdOpts.error('Project name must be specified. See options -p and --project')
     if len(args) != 1:
         cmdOpts.error('There must be one and only one positional argument of action.')
-    
+
     # Create RPC proxy.
     host = cfg.get('rpc-client', 'server')
     port = cfg.getint('rpc-server', 'port')
@@ -83,13 +83,13 @@ addJob (a), deleteJob (delj), runJob (r)."""
             logger.error(msg % options.proj)
         else:
             sp.createProject(options.proj)
-            if not sp.projectExists(options.project):
+            if not sp.projectExists(options.proj):
                 logger.warning("Error adding project '%s'." % options.proj)
     elif args[0] == 'delp' or args[0] == 'deleteProject':
         logger.info('Deleting project: %s' % options.proj)
         if sp.projectExists(options.proj):
             sp.deleteProject(options.proj)
-            if sp.projectExists(options.project):
+            if sp.projectExists(options.proj):
                 logger.warning("Error deleting project '%s'." % options.proj)
         else:
             msg = "Couldn't delete project '%s' that doesn't exists."
@@ -97,9 +97,9 @@ addJob (a), deleteJob (delj), runJob (r)."""
     elif args[0] == 'a' or args[0] == 'addJob':
         if len(options.job) == 0: cmdOpts.error('Job name must be specified.')
         if len(options.file) == 0: cmdOpts.error('Job file must be specified.')
-        logger.info("Adding job '%s' to project '%s' from file '%s'" % 
+        logger.info("Adding job '%s' to project '%s' from file '%s'" %
                     (options.job, options.proj, options.file))
-                    
+
         if not sp.projectExists(options.proj):
             msg = "Couldn't add job to project '%s' because project doesn't exists."
             logger.error(msg % options.proj)
@@ -108,16 +108,20 @@ addJob (a), deleteJob (delj), runJob (r)."""
             msg = "Couldn't add job '%s' to project '%s' because job already exists."
             logger.error(msg % (options.job, options.proj))
             sys.exit(1)
-            
-        sp.addJob(option.proj, options.job, yamlStr)
-        if not sp.jobExists(options.project, options.job):
+
+        f = open(options.file, 'r')
+        yamlStr = f.read(-1)
+        f.close()
+
+        sp.addJob(options.proj, options.job, yamlStr)
+        if not sp.jobExists(options.proj, options.job):
             msg = "Error adding job '%s' to project '%s'."
             logger.warning(msg % (options.job, options.proj))
-            
+
     elif args[0] == 'delj' or args[0] == 'deleteJob':
         if len(options.job) == 0: cmdOpts.error('Job name must be specified.')
         logger.info("Deleting job '%s' from project '%s'" % (options.job, options.proj))
-                    
+
         if not sp.projectExists(options.proj):
             msg = "Couldn't delete job from project '%s' because project doesn't exists."
             logger.error(msg % options.proj)
@@ -125,14 +129,14 @@ addJob (a), deleteJob (delj), runJob (r)."""
             msg = "Couldn't delete job '%s' from project '%s' because job doesn't exists."
             logger.error(msg % (options.job, options.proj))
         else:
-            sp.deleteJob(options.project, options.job)
-            if sp.jobExists(options.project, options.job):
+            sp.deleteJob(options.proj, options.job)
+            if sp.jobExists(options.proj, options.job):
                 msg = "Error deleting job '%s' from project '%s'."
                 logger.warning(msg % (options.job, options.proj))
     elif args[0] == 'r' or args[0] == 'runJob':
         if len(options.job) == 0: cmdOpts.error('Job name must be specified.')
         logger.info("Running job '%s' from project '%s'" % (options.job, options.proj))
-                    
+
         if not sp.projectExists(options.proj):
             msg = "Couldn't run job from project '%s' because project doesn't exists."
             logger.error(msg % options.proj)
@@ -140,30 +144,7 @@ addJob (a), deleteJob (delj), runJob (r)."""
             msg = "Couldn't run job '%s' from project '%s' because job doesn't exists."
             logger.error(msg % (options.job, options.proj))
         else:
-            sp.runJob(options.project, options.job)
+            sp.runJob(options.proj, options.job)
 
     # Say hello.
     # print( sp.hello('Hello Mr. Server!') )
-
-    # Model file.
-    modelFile = args[0]
-    modelName = args[1]
-    logger.info('Working with model file: %s' % modelFile)
-    fp = open(modelFile, 'r')
-    yamlStr = fp.read(-1)
-    fp.close()
-    logger.info('Checking whether project already exists on server.')
-    if not sp.projectExists(modelName):
-        logger.info('Adding project to database.')
-        sp.createProject(modelName)
-        sp.addJob(modelName, modelName, yamlStr)
-        #sp.simulate(modelName, '1')
-    elif options.force:
-        logger.info('Project exists and --force is selected. Remove current project.')
-        sp.deleteProject(modelName)
-        sp.createProject(modelName)
-        sp.addJob(modelName, modelName, yamlStr)
-        sp.simulate(modelName, '1')
-    else:
-        logger.error(("Couldn't add model with name '%s' to server " +
-                     "because model with this name alredy exists.") % modelName)
