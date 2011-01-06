b070c5d9935c1f94a1a60850b2e9991b3bdc5bee
anthony kolesov <anthony.kolesov@gmail.com>
2011-01-06 21:53:50 +0300

    Update copyright years.

--- lib/kts46/simulationServer.py
+++ lib/kts46/simulationServer.py
@@ -1,6 +1,6 @@
 """
 License:
-   Copyright 2010 Anthony Kolesov
+   Copyright 2010-2011 Anthony Kolesov
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
