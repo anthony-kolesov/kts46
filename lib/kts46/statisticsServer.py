dc4dbb7b03afe26acbb30511733e06787988a5ef
anthony kolesov <anthony.kolesov@gmail.com>
2011-01-09 18:37:16 +0300

    Improve basicStats view.

--- lib/kts46/statisticsServer.py
+++ lib/kts46/statisticsServer.py
@@ -17,29 +17,19 @@ License:
 
 import json, math, numpy, logging
 from urllib2 import urlopen
-from kts46.CouchDBStorage import CouchDBStorage
 
 class StatisticsServer:
 
     def __init__(self, cfg):
         self.cfg = cfg
         self.log = logging.getLogger(cfg.get('loggers', 'StatisticsServer'))
-        self.storage = CouchDBStorage(cfg.get('couchdb', 'dbaddress'))
-
-    def _getJSON(self, url):
-        a = urlopen(url)
-        text = a.read(None).decode("UTF-8", "strict")
-        a.close()
-        return json.loads(text)
 
     def calculate(self, job):
-        startKey = [job.id]
-        endKey = [job.id + 1]
-        addCarData = job.project.db.view("basicStats/addCar")[startKey:endKey]
-        delCarData = job.project.db.view("basicStats/deleteCar")[startKey:endKey]
+        addCarData = job.project.db.view("basicStats2/addCar")[job.id]
+        delCarData = job.project.db.view("basicStats2/deleteCar")[job.id]
 
-        addCarTimes = dict((x['key'][1], x['value']['time']) for x in addCarData)
-        delCarTimes = dict((x['key'][1], x['value']['time']) for x in delCarData)
+        addCarTimes = dict((x['value']['car'], x['value']['time']) for x in addCarData)
+        delCarTimes = dict((x['value']['car'], x['value']['time']) for x in delCarData)
 
         times = {}
         moveTimes = []
@@ -57,5 +47,5 @@ class StatisticsServer:
         job.statistics['average'] = av if not math.isnan(av) else - 1
         job.statistics['stdeviation'] = stdd
         job.statistics['finished'] = True
-        job.save()
+        #job.save()
 
