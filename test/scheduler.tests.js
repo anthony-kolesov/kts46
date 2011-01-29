935623999e3a58a03a6a4a9dbdff80d0df7d0639
anthony kolesov <anthony.kolesov@gmail.com>
2011-01-29 23:58:24 +0300

    Tests of addTask.

--- test/scheduler.tests.js
+++ test/scheduler.tests.js
@@ -3,22 +3,50 @@ var RPCClient = require('../jslib/jsonrpc-client').Client,
     assert = require('assert');
 
 // Setup assert.
-//as.isString = function(value, msg) {
-//    as.ok(value instanceof String, msg)
-//};
-
+assert.isString = function(value, msg) {
+    assert.ok(typeof(value) === "string", msg)
+};
+assert.isNull = function(val, msg) {
+    assert.strictEqual(val, null, msg);
+};
+assert.isNotNull = function(val, msg) {
+    assert.notStrictEqual(val, null, msg);
+};
 
 var getClient = function() {
     return new RPCClient('192.168.1.2', 46212);
 }
 var client = getClient();
 
+// Locals
+var existingProject = 'node_test',
+    existingJobDone = 'node_test_done',
+    existingJobUndone = 'undone',
+    unexistingProject = 'QQQSDFSDFq3498',
+    unexistingJob = 'QQQSDFSDFq3498sfsdf';
 
 var onHello = function(data) {
-    console.log('aa');
-    assert.ok(data instanceof Number, "sdasdasd");
+    assert.isNull(data.error, "There mustn't be errors.");
+    assert.isString(data.result, "Return result must be string.");
+    process.nextTick(startAddTask);
 };
 
 
-client.call('hello', [], onHello);
-// callServer('hello', [], onHello);
+var startAddTask = function() {
+    client.call('addTask', existingProject, existingJobUndone, onTaskAdded);
+};
+var onTaskAdded = function(data) {
+    assert.isNull(data.error);
+    assert.strictEqual(data.result, "success");
+    process.nextTick(addTaskAgain);
+};
+var addTaskAgain = function() {
+    client.call('addTask', existingProject, existingJobUndone, onTaskAddedDuplicate);
+};
+var onTaskAddedDuplicate = function(data) {
+    assert.isNotNull(data.error);
+    assert.isNull(data.result);
+    assert.strictEqual(data.error.type, 'DuplicateTask');
+};
+
+client.call('hello', onHello);
