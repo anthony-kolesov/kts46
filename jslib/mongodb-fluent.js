6f9686f99efd80790818cd7b38b2e18c35fa943a
anthony kolesov <anthony.kolesov@gmail.com>
2011-01-30 23:27:20 +0300

    Move findindb to module.

--- jslib/mongodb-fluent.js
+++ jslib/mongodb-fluent.js
@@ -4,10 +4,10 @@ var mongodb = require('./mongodb');
 
 var ErrorType = 'MongoDBError';
 
-function Client(server) {
+/*var Client = function(server) {
     this.server = server;
     this.nativeParser = true;
-}
+}*/
 
 
 var onMongodbError = function(err, client, onError){
@@ -15,22 +15,23 @@ var onMongodbError = function(err, client, onError){
         process.nextTick(onError.bind({},{type: ErrorType, msg: err.message}));
     }
     client.close();
+    console.log("mongo error");
     console.log(err);
+    console.log("mongo error end");
 };
 
 
-Client.prototype.getDbClient = function(dbName) {
-    return new mongodb.Db(dbName, this.server, {native_parser: this.nativeParser});
+var getDbClient = function(server, dbName) {
+    return new mongodb.Db(dbName, server, {native_parser: true});
 };
 
 // Finds documents in database and provides cursor to callback as an arguments
 // onFinished: function(cursor)
 // onError: function(errorObject)
-Client.prototype.find = function(db, collectionName, spec, fields, onFinished, onError) {
-    var client = this.getDbClient(db);
+var find = function(client, collectionName, spec, fields, onFinished, onError) {
+    //var client = this.getDbClient(db);
     client.open(function(err, pClient) {
         if (err) { onMongodbError(err, client, onError); return; }
-         throw ; }
         client.collection(collectionName, function(err, collection) {
             if (err) { onMongodbError(err, client, onError); return; }
             collection.find(spec, fields, function(err, cursor){
@@ -46,16 +47,19 @@ Client.prototype.find = function(db, collectionName, spec, fields, onFinished, o
 // Update documents in database.
 // onFinished: function()
 // onError: function(errorObject)
-Client.prototype.updateDocuments = function(db, collectionName, spec, changes, options, onFinished, onError) {
-    var client = this.getDbClient(db);
+var update = function(client, db, collectionName, spec, changes, options, onFinished, onError) {
+    // var client = this.getDbClient(db);
     client.open(function(err, pClient) {
-        if (err) { onMongodbError.(err, client, onError); return; }
+        if (err) { onMongodbError(err, client, onError); return; }
         client.collection(collectionName, function(err, collection) {
-            if (err) { onMongodbError.(err, client, onError); return; }
+            if (err) { onMongodbError(err, client, onError); return; }
             collection.update(spec, changes, options, function(err, cursor){
-                if (err) {  onMongodbError.(err, client, onError); return; }
+                if (err) {  onMongodbError(err, client, onError); return; }
                 if (onFinished) process.nextTick(onFinished);
             });
         } );
     });
 };
+
+exports.find = find;
+exports.update = update;
