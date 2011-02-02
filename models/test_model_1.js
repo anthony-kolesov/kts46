4dce09c18eacba715b1a20e417f2100bc2d3f94a
anthony kolesov <anthony.kolesov@gmail.com>
2011-02-03 00:20:18 +0300

    Store model as JSON.

---
+++ models/test_model_1.js
@@ -0,0 +1,33 @@
+{
+    "simulationParameters": {
+        "duration": 90,
+        "stepDuration": 0.2,
+        "batchLength": 50
+    },
+
+    "modelParameters": {
+        "inputRate": 1200,
+        "safeDistance": 10,
+        "maxSpeed": 20,
+        "minSpeed": 10
+    },
+
+    "road": {
+        "length": 300,
+        "width": 10,
+        "lines": 3
+    },
+
+    "trafficLights": {
+        "1": { "id": "1",
+           "position": 80,
+           "greenDuration": 5,
+           "redDuration": 4
+        },
+        "2": {  "id": "2",
+           "position": 220,
+           "greenDuration": 5,
+           "redDuration": 9
+        }
+    }
+}
