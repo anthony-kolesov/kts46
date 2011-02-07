78ccb2faaa3712a70e803f6801aa8ddbc5d62229
anthony kolesov <anthony.kolesov@gmail.com>
2011-02-08 00:22:01 +0300

    Convert scheduler wrapper to class.

--- bin/controlNode.js
+++ bin/controlNode.js
@@ -10,21 +10,23 @@ var cfg = {
   port: 46212,
   address: "",
   jsonRpcPath: "/jsonrpc",
-  debugRpc: false,
+  debugRpc: true,
   mongodbAddress: ['192.168.1.5', 27017]
 };
 // Configure scheduler module.
-scheduler.cfg.mongodbAddress = cfg.mongodbAddress;
+// scheduler.cfg.mongodbAddress = cfg.mongodbAddress;
 
 // Local variables
 var version = "0.1.4"
     versionString = "ControlNode " + version + " on node.js";
 
+var schedulerWrapper = new scheduler.Wrapper();
+    
 var handleHttpRequest = function(req, res) {
   if(req.method == "POST"){
     // Define whether this is JSON request.
     if (urllib.parse(req.url).pathname == cfg.jsonRpcPath) {
-      new RPCHandler(req, res, scheduler.rpcMethods, cfg.debugRpc);
+      new RPCHandler(req, res, schedulerWrapper, cfg.debugRpc);
     } else {
       res.writeHead(404, {'Content-Type': 'text/plain'});
       res.end('Unknown path!\n');
--- jslib/schedulerRpcWrapper.js
+++ jslib/schedulerRpcWrapper.js
@@ -1,27 +1,27 @@
-var SchedulerLib = require('../jslib/scheduler');
+var SchedulerLib = require('./scheduler');
 
 // Scheduler constants
 var taskType = SchedulerLib.taskTypes;
 
 
-// Module locals
-var cfg = {};
-var scheduler = new SchedulerLib.Scheduler();
+var Wrapper = function() {
+    this.scheduler = new SchedulerLib.Scheduler();
+};
 
 
 // Util methods.
-var checkType = function(rpc, arg, argname, type) {
-  if (typeof(arg) !== type) {
-    var err = {"type": "InvalidArgumentType", "argumentName": argname};
-    rpc.error(err);
-    return true;
-  }
-  return false;
+Wrapper.prototype._checkType = function(rpc, arg, argname, type) {
+    if (typeof(arg) !== type) {
+        var err = {"type": "InvalidArgumentType", "argumentName": argname};
+        rpc.error(err);
+        return true;
+    }
+    return false;
 };
 
 
 /* Ensures that provided taskTypes are correct. */
-var parseTaskTypes = function(taskTypes, rpc) {
+Wrapper.prototype._parseTaskTypes = function(taskTypes, rpc) {
     var effTaskTypes = taskTypes;
 
     // Is string, undefined or array.
@@ -34,7 +34,7 @@ var parseTaskTypes = function(taskTypes, rpc) {
 
     for (var i=0, l=effTaskTypes.length; i < l; ++i ) {
         if (effTaskTypes[i] !== null) {
-            if ( checkType(rpc, effTaskTypes[i], ["taskTypes[", i, "]"].join(""), "string") )
+            if ( this._checkType(rpc, effTaskTypes[i], ["taskTypes[", i, "]"].join(""), "string") )
                 return null;
             if ( !taskType.hasOwnProperty(effTaskTypes[i]) ) {
                 rpc.error({'type': 'UnknownTaskType', 'taskType': effTaskTypes[i],
@@ -48,98 +48,90 @@ var parseTaskTypes = function(taskTypes, rpc) {
 };
 
 
-// Types
-var SchedulerContext = function(jsonRpcResponse) {
-    this.response = jsonRpcResponse;
-};
-
-
 // RPC methods.
-var hello = function(rpc){
-  rpc.response("Hello! I am ControlNode on node.js");
+Wrapper.prototype.hello = function(rpc){
+    rpc.response("Hello! I am ControlNode on node.js");
 };
 
 
-var addTask = function(rpc) {
+Wrapper.prototype.addTask = function(rpc) {
   var projectName = arguments[1],
       jobName = arguments[2];
 
     // Check arg types.
-    if (checkType(rpc, projectName, "projectName", "string") ||
-        checkType(rpc, jobName, "jobName", "string") ) {
+    if (this._checkType(rpc, projectName, "projectName", "string") ||
+        this._checkType(rpc, jobName, "jobName", "string") ) {
       return;
     }
 
-    var taskTypes = parseTaskTypes(arguments[3], rpc);
+    var taskTypes = this._parseTaskTypes(arguments[3], rpc);
     if (taskTypes == null) return;
     
-    scheduler.addTask(rpc, projectName, jobName, taskTypes);
-    // var context = new SchedulerContext(rpc);
-    // addTaskImplementation.bind(context)(projectName, jobName, taskTypes);
+    this.scheduler.addTask(rpc, projectName, jobName, taskTypes);
 };
 
 
-var abortTask = function(rpc, projectName, jobName) {
-    if (checkType(rpc, projectName, "projectName", "string") ||
-        checkType(rpc, jobName, "jobName", "string") ) {
+Wrapper.prototype.abortTask = function(rpc, projectName, jobName) {
+    if (this._checkType(rpc, projectName, "projectName", "string") ||
+        this._checkType(rpc, jobName, "jobName", "string") ) {
         return;
     }
 
-    scheduler.abortTask(rpc, projectName, jobName);
+    this.scheduler.abortTask(rpc, projectName, jobName);
 };
 
 
-var getTask = function(rpc, workerId){
-    if (checkType(rpc, workerId, "workerId", "string")) {
+Wrapper.prototype.getTask = function(rpc, workerId){
+    if (this._checkType(rpc, workerId, "workerId", "string")) {
         return;
     }
-    var taskTypes = parseTaskTypes(arguments[2], rpc);
+    var taskTypes = this._parseTaskTypes(arguments[2], rpc);
     if (taskTypes === null) return;
-    scheduler.getTask(rpc, workerId, taskTypes);
+    this.scheduler.getTask(rpc, workerId, taskTypes);
 };
 
 
-var acceptTask = function(rpc, workerId, sig){
-    if (checkType(rpc, workerId, "workerId", "string") ||
-        checkType(rpc, sig, "sig", "string") ) {
+Wrapper.prototype.acceptTask = function(rpc, workerId, sig){
+    if (this._checkType(rpc, workerId, "workerId", "string") ||
+        this._checkType(rpc, sig, "sig", "string") ) {
         return;
     }
-    scheduler.acceptTask(rpc, workerId, sig);
+    this.scheduler.acceptTask(rpc, workerId, sig);
 };
 
 
-var rejectTask = function(rpc, workerId, sig) {
-    if (checkType(rpc, workerId, "workerId", "string") ||
-        checkType(rpc, sig, "sig", "string") ) {
+Wrapper.prototype.rejectTask = function(rpc, workerId, sig) {
+    if (this._checkType(rpc, workerId, "workerId", "string") ||
+        this._checkType(rpc, sig, "sig", "string") ) {
         return;
     }
-    scheduler.rejectTask(rpc, workerId, sig);
+    this.scheduler.rejectTask(rpc, workerId, sig);
 };
 
 
-var taskFinished = function(rpc, workerId, sig){
-    if (checkType(rpc, workerId, "workerId", "string") ||
-        checkType(rpc, sig, "sig", "string") ) {
+Wrapper.prototype.taskFinished = function(rpc, workerId, sig){
+    if (this._checkType(rpc, workerId, "workerId", "string") ||
+        this._checkType(rpc, sig, "sig", "string") ) {
         return;
     }
-    scheduler.taskFinished(rpc, workerId, sig);
+    this.scheduler.taskFinished(rpc, workerId, sig);
 };
 
 
-var taskInProgress = function(rpc, workerId, sig){
-    if (checkType(rpc, workerId, "workerId", "string") ||
-        checkType(rpc, sig, "sig", "string") ) {
+Wrapper.prototype.taskInProgress = function(rpc, workerId, sig){
+    if (this._checkType(rpc, workerId, "workerId", "string") ||
+        this._checkType(rpc, sig, "sig", "string") ) {
         return;
     }
-    scheduler.taskInProgress(rpc, workerId, sig);
+    this.scheduler.taskInProgress(rpc, workerId, sig);
 };
 
 
-var getCurrentTasks = function(rpc) {
-    scheduler.getCurrentTasks(rpc);
+Wrapper.prototype.getCurrentTasks = function(rpc) {
+    this.scheduler.getCurrentTasks(rpc);
 };
 
-var restartTasks = function(rpc, tasks) {
+Wrapper.prototype.restartTasks = function(rpc, tasks) {
     if (!Array.isArray(tasks)) {
         rpc.error({'type':'InvalidArgumentType', 'argumentName': 'tasks'});
         return;
@@ -152,13 +144,13 @@ var restartTasks = function(rpc, tasks) {
         }
     });
 
-    scheduler.restartTasks(rpc, tasks);
+    this.scheduler.restartTasks(rpc, tasks);
 };
 
-
-exports.rpcMethods = {'hello':hello, 'addTask':addTask, 'abortTask':abortTask,
+exports.Wrapper = Wrapper;
+/*exports.rpcMethods = {'hello':hello, 'addTask':addTask, 'abortTask':abortTask,
     'getTask': getTask, 'acceptTask': acceptTask, 'rejectTask': rejectTask,
     'taskFinished': taskFinished, 'taskInProgress': taskInProgress,
     'getCurrentTasks': getCurrentTasks, 'restartTasks': restartTasks
 };
-exports.cfg = cfg;
+exports.cfg = cfg;*/
