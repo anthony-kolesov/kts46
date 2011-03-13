1df1407ba986b5574c5206ca0290ee95ade885f0
anthony kolesov <anthony.kolesov@gmail.com>
2011-03-13 23:24:46 +0300

    Improved BOINC client.

--- bin/boinc_worker.py
+++ bin/boinc_worker.py
@@ -1,24 +1,33 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
+# Copyright 2010-2011 Anthony Kolesov
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import boinc
 import csv
 import sys
 import tarfile
 import yaml
-import time
-import random
-import math
-from pymw import pymw
-from pymw import interfaces
+from ConfigParser import SafeConfigParser
 from optparse import OptionParser
-#import boinc
 
 # Project imports
-#PROJECT_LIB_PATH = '/home/anthony/kts46.boinc/lib/'
+#PROJECT_LIB_PATH = '../../'
 #if PROJECT_LIB_PATH not in sys.path:
 #    sys.path.append(PROJECT_LIB_PATH)
-#import kts46
-import kts46.simulationServer
-import kts46.offline_support
+from kts46.simulationServer import SimulationServer
+
 
 def configureCmdOptions():
     usage = "usage: %prog [options] modelFile.yaml"
@@ -27,62 +36,91 @@ def configureCmdOptions():
     cmdOpts.add_option('-s', '--states', action='store', dest='statesFile',
         default='states.csv',
         help="Output file for states." )
-    cmdOpts.add_option('-v', '--cars', action='store', dest='carsFile',
+    cmdOpts.add_option('-c', '--cars', action='store', dest='carsFile',
         default='cars.csv',
         help="Output file for cars." )
 
-    return interfaces.parse_options(cmdOpts)
+    return cmdOpts.parse_args(sys.argv[1:])
 
 
-def runTask(definition):
-    ss = kts46.simulationServer.SimulationServer()
-    storage = kts46.offline_support.MemoryStateStorage(definition['simulationParameters']['batchLength'])
-    job = kts46.offline_support.OfflineJob(definition)
-    ss.runSimulationJob(job, storage)
-    return storage
+class OfflineJob(object):
 
-        
+    def __init__(self, definition):
+        self.currentFullState = None
+        self.definition = definition
+        self.progress = {}
 
+    def saveSimulationProgress(self, stepsDone):
+        pass
+
+class CSVStateStorage(object):
+
+    def __init__(self, statesFile, carsFile, totalSteps):
+        self.totalSteps = totalSteps
+        self.stepsDone = 0
+        self.statesWriter = csv.writer(statesFile, quoting=csv.QUOTE_MINIMAL)
+        self.carsWriter = csv.writer(carsFile, quoting=csv.QUOTE_MINIMAL)
+
+        self.statesWriter.writerow( ["Time", "TimeAsTD",
+            "Last car generation time", "Last car id"] )
+        self.carsWriter.writerow(["Time", "Car id", "Desired speed",
+            "Current speed", "Position", "Line" ])
+
+
+    def add(self, time, data):
+
+        # time as seconds, time as dt, lastCarGenerationTime
+        state = [
+            time,
+            data['time'],
+            data['lastCarGenerationTime'],
+            data['lastCarId']
+        ]
+        self.statesWriter.writerow(state)
+
+        for carId, car in data['cars'].items():
+            row = [
+                time,
+                carId,
+                car['desiredSpeed'],
+                car['curspd'],
+                car['pos'],
+                car['line'],
+                car['width'],
+                car['length']
+            ]
+            self.carsWriter.writerow(row)
+
+        self.stepsDone += 1
+        boinc.set_fraction_done( float(self.stepsDone) / self.totalSteps)
+        #if (self.stepsDone % 100 == 0) or (self.stepsDone == self.totalSteps):
+        #    with open("done.txt", "w") as f:
+        #        f.write( str(float(self.stepsDone) / self.totalSteps) )
+
+    def close(self):
+        pass
+
+    def repair(self, currentTime):
+        pass
+
+boinc.init()
+cfg = SafeConfigParser()
 options, args = configureCmdOptions()
-inputFilePath = args[0]
-statesFilePath = options.statesFile
-carsFilePath = options.carsFile
+inputFilePath = boinc.resolve_filename(args[0])
+statesFilePath = boinc.resolve_filename(options.statesFile)
+carsFilePath = boinc.resolve_filename(options.carsFile)
 
 with open(inputFilePath) as f:
     definition = yaml.load(f.read())
 
-    
-# get an interface object based on command-line options 
-interface_obj = interfaces.get_interface(options)
-if hasattr(interface_obj, "set_boinc_args"):
-    interface_obj.set_boinc_args(1, 1, int(1e6))
-
-pymw_master = pymw.PyMW_Master(interface=interface_obj)
-
-tasks = [pymw_master.submit_task(runTask,
-                                input_data=(definition, ),
-                                modules=("kts46.simulationServer","kts46.offline_support", "yaml","jsonRpcClient"),
-                                dep_funcs=())]
-
-                                
-res_task, result = pymw_master.get_result()
-
-# Write result.
+ss = SimulationServer(cfg)
 statesFile = open(statesFilePath, "wb")
 carsFile = open(carsFilePath, "wb")
 
-statesWriter = csv.writer(statesFile, quoting=csv.QUOTE_MINIMAL)
-carsWriter = csv.writer(carsFile, quoting=csv.QUOTE_MINIMAL)
-statesWriter.writerow( ["Time", "TimeAsTD",
-    "Last car generation time", "Last car id"] )
-carsWriter.writerow(["Time", "Car id", "Desired speed",
-    "Current speed", "Position", "Line" ])
+storage = CSVStateStorage(statesFile, carsFile, definition['simulationParameters']['batchLength'])
+job = OfflineJob(definition)
+ss.runSimulationJob(job, storage)
 
-for car in result.cars:
-    carsWriter.writerow(car)
-for state in result.states:
-    statesWriter.writerow(state)
-    
 # Close files for writing
 statesFile.close()
 carsFile.close()
@@ -91,12 +129,11 @@ carsFile.close()
 statesFile = open(statesFilePath, "rb")
 carsFile = open(carsFilePath, "rb")
 
-tar = tarfile.open("data.tar.bz2", "w:bz2")
+tar = tarfile.open(boinc.resolve_filename("data.tar.bz2"), "w:bz2")
 tar.addfile(tar.gettarinfo(fileobj=statesFile), statesFile)
 tar.addfile(tar.gettarinfo(fileobj=carsFile), carsFile)
 
 tar.close()
 statesFile.close()
 carsFile.close()
-
-print("Task has been done!")
+boinc.finish(0)
