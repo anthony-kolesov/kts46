8ca4765abda5767a730da2df7b21739f91915c06
anthony kolesov <anthony.kolesov@gmail.com>
2011-03-17 22:51:34 +0300

    Simple logging to control node.

--- ControlNode/controlNode.js
+++ ControlNode/controlNode.js
@@ -3,40 +3,56 @@ var http = require('http'),
     urllib = require('url'),
     RPCHandler = require('jsonrpc').RPCHandler,
     scheduler = require('./schedulerRpcWrapper'),
-    handleStaticFile = require('./staticFileHandler').handle;
+    handleStaticFile = require('./staticFileHandler').handle,
+    fs = require('fs');
 
 // Configuration
 var cfg = require('config')('ControlNode', {
   port: 46212,
   address: "",
   jsonRpcPath: "/jsonrpc",
-  debugRpc: false
+  debugRpc: false,
+  statusPath: "/status",
+  webuiFilesPath: "../http_server/web",
+  webuiPathRoot: ""
 });
 
 // Local variables
 var version = "0.1.6",
-    versionString = ["ControlNode=", version, ";NodeJS=", process.version].join("");
+    versionString = ["ControlNode=", version, ";NodeJS=", process.version].join(""),
+    logfile = fs.createWriteStream("/var/log/kts46/ControlNode.log", {flags:"w+"});
+
+var log = function(code, path){
+    logfile.write([Date(), code, path + "\n"].join("\t"));
+};
+
 
 var schedulerWrapper = new scheduler.Wrapper(cfg.mongodbAddress);
 
 var handleHttpRequest = function(req, res) {
+    var path = urllib.parse(req.url).pathname;
     if(req.method == "POST"){
         // Define whether this is JSON request.
-        if (urllib.parse(req.url).pathname == cfg.jsonRpcPath) {
+        if (path == cfg.jsonRpcPath) {
+            log(200, path);
             new RPCHandler(req, res, schedulerWrapper, cfg.debugRpc);
         } else {
+            log(404, path);
             res.writeHead(404, {'Content-Type': 'text/plain'});
             res.end('Unknown path!\n');
         }
-    } else if (urllib.parse(req.url).pathname === "/status") {
+    } else if (path === cfg.statusPath) {
+        log(200, path);
         res.writeHead(200, {'Content-Type': 'application/json'});
         res.end(JSON.stringify(schedulerWrapper.getStatus(), null, 4))
     } else {
-        handleStaticFile('../http_server/web', '', req, res);
+        log(200, path);
+        handleStaticFile(cfg.webuiFilesPath, cfg.webuiPathRoot, req, res);
     }
 };
 
 
 // Run server.
 http.createServer(handleHttpRequest).listen(cfg.port, cfg.address);
-console.log([versionString, ";at http://", cfg.address, ':', cfg.port].join(''));
+console.log(["Starting ", versionString, " at http://", cfg.address, ':', cfg.port].join(''));
+logfile.write([Date()])
