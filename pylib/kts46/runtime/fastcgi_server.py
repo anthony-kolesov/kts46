fab8120da42646643554a4e9596b9a1c79eec1c9
anthony kolesov <anthony.kolesov@gmail.com>
2011-03-25 01:43:32 +0300

    Management API.

--- pylib/kts46/runtime/fastcgi_server.py
+++ pylib/kts46/runtime/fastcgi_server.py
@@ -4,7 +4,7 @@
 from cgi import escape
 import sys, os
 from flup.server.fcgi import WSGIServer
-from kts46.server.webui import DataAPIHandler
+from kts46.server.webui import DataAPIHandler, ManagementAPIHandler
 import kts46.utils
 
 # Create ConfigParser
@@ -15,12 +15,18 @@ cfg = kts46.utils.getConfiguration([]) # configFiles)
 #    cfg.set('log', 'quite', str(options.quite))
 
 dh = DataAPIHandler(cfg)
-
+mh = ManagementAPIHandler(cfg)
 
 def app(environ, startResponse):
-    if environ["SCRIPT_NAME"] == "/api/data":
+    sfn = environ["SCRIPT_FILENAME"]
+    if sfn == "/api/data":
         return dh.handle(environ, startResponse)
+    elif sfn == "/api/jsonrpc":
+        return mh.handle(environ, startResponse)
     else:
-        return dh.handle(environ, startResponse)
+        startResponse('200 OK', [("Content-Type", "text/plain")])
+        for k, v in environ.iteritems():
+            print("%s -> %s" % (k, v))
+        return ""
 
 WSGIServer(app, bindAddress="/tmp/kts46_fcgi.socket", umask=0000).run()
