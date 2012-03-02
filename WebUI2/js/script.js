b8777c22614abc4dbe83a8db3f226466a766a31b
Anthony Kolesov <anthony.kolesov+bitbucket@gmail.com>
2012-03-02 19:20:08 +0400

    Adding view generator.

--- WebUI2/js/script.js
+++ WebUI2/js/script.js
@@ -9,13 +9,16 @@
     }
 
 
-    function proc(p) {
+    function proc(cars, p) {
         var viewParams = this.view
           , model = this
           , scale = viewParams.scale
           , startx, starty, endx, endy, startName, endName, startPoint, endPoint, roads, i
           , roadx, roady, roadWidth, roadHeight, road
+        p.draw = drawFrame.bind(p, model, cars)
         p.setup = function() {
+            p.frameRate(1 / model.simulationParameters.stepDuration)
+
             p.size(viewParams.size.x * scale, viewParams.size.y * scale)
             p.background(200)
             p.stroke(0)
@@ -44,14 +47,27 @@
                     roady = starty - roadHeight / 2
                 }
                 p.rect(roadx, roady, roadWidth, roadHeight)
-            } 
+                // Save for future use
+                road.view = {start: {x: startx, y: starty}, end: {x: endx, y: endy} }
+            }
         }
     }
 
-    function drawModel(model) {
+
+    function drawFrame(model, cars) {
+        var currentCars = cars.shift()
+          , i, l, road, car
+        for (i = 0, l = currentCars.length; i < l; ++i) {
+            car = currentCars[i]
+            road = model.roads[car.road || 0]
+        }
+        //this.point(model.pointx++, model.pointy++)
+    }
+
+
+    function drawModel(model, cars) {
         var canvas = document.getElementById('drawboard')
-          , p = new Processing(canvas, proc.bind(model) )
-        p.exit()
+          , p = new Processing(canvas, proc.bind(model, cars) )
     }
 
     $(document).ready(function(){
@@ -59,11 +75,15 @@
             var file = document.getElementById('model-file').files[0]
               , reader = new FileReader()
             reader.onload = function(e) {
-                drawModel(JSON.parse(e.target.result))
+                var carsFile = document.getElementById('cars-file').files[0]
+                  , carsReader = new FileReader()    
+                carsReader.onload = function(carsEvent) {
+                    drawModel(JSON.parse(e.target.result), JSON.parse(carsEvent.target.result))
+                }
+                carsReader.onerror = console.log
+                carsReader.readAsText(carsFile)
             }
-            reader.onerror = function(e) {
-                console.log(e)
-            }
+            reader.onerror = console.log(e)}
             reader.readAsText(file)
         })
     })
---
+++ pylib/kts46/model/Crossroad.py
@@ -0,0 +1,27 @@
+# Copyright 2010-2012 Anthony Kolesov
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+class Crossroad(object):
+    "Defines an endpoint where cars come in and come out of model."
+
+    def __init__(self, name, coords, trafficLight=None):
+        "Creates new endpoint."
+        self.name = name
+        self.coords = coords
+        self.road = None
+        self.trafficLight = trafficLight
+
+
+    def getDescriptionData(self):
+        return {'name': self.name}
